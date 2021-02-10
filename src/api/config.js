3a44270109f845cba57a132a13e681c3f0d3b36b
Davide Plessi <davideplessi@gmail.com>
2021-02-10 22:58:07 +0100

    fixes

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
-//export const API_BASE_URL = "https://fanta-master-chef.herokuapp.com/api/";
-export const API_BASE_URL = "http://localhost:5001/api/";
+export const API_BASE_URL = "https://fanta-master-chef.herokuapp.com/api/";
+//export const API_BASE_URL = "http://localhost:5001/api/";
 export const ENABLE_ERROR_LOG = false;
